d745e47c992bd05119053f69ee0daba933cac6da
Jordan Kline <jordan.kline@gmail.com>
2018-01-07 04:54:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.scan(/ ([aeiouAEIOU])/)
+  if word.scan(/ ([aeiouAEIOU])/)
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
