1b52e4257185deaf0650b725a458a2633b94d981
Jordan Kline <jordan.kline@gmail.com>
2018-01-07 05:24:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  x = text.scan(/\A[A-Z].+[.?!]/).flatten.length > 0
+  x = text.scan(/\A[A-Z].+[.?!]/)
   binding.pry
   #
   #   return true
