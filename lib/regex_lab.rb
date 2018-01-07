edca3429528593aa19e650248a876fb62dc24664
Jordan Kline <jordan.kline@gmail.com>
2018-01-07 05:38:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,6 +17,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  binding.pry
-  phone.scan(/ /)
+  phone.scan(/\D?(\d{3})\D?(\d{3})\D?(\d{4})/)
 end
