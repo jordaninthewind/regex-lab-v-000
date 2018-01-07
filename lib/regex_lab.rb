8431f8c45cb907d3cc7acd22d14712837f1d65e8
Jordan Kline <jordan.kline@gmail.com>
2018-01-07 05:08:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.scan(/[aeiouAEIOU]\w+/).length > 0
+  word.scan(/[aeiouAEIOU]\w+/).length > 0 ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
