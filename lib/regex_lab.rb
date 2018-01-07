0431b7b8156407d1d259c6c1247bce3f1b6d24bd
Jordan Kline <jordan.kline@gmail.com>
2018-01-07 04:57:13 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.scan(/[aeiouAEIOU]\w+/) ? true : false
+  word.scan(/[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
